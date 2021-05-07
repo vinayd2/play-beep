8b06d26b520c8cf49790c6fe49d08ceab9dc2ef8
vinayd2 <vinay_002@yahoo.com>
2021-05-07 23:54:11 +0530

    initial commit

---
+++ beep.py
@@ -0,0 +1,27 @@
+import sys, os
+
+WARNING_BEEP_DURATION = (1000, 2000)
+
+try:
+    import winsound
+
+except ImportError:
+    import os
+
+    if sys.platform == "darwin":
+
+        def beep(freq, duration):
+            # brew install SoX --> install SOund eXchange universal sound sample translator on mac
+            os.system(
+                f"play -n synth {duration/1000} sin {freq} >/dev/null 2>&1")
+    else:
+
+        def beep(freq, duration):
+            # apt-get install beep  --> install beep package on linux distros before running
+            os.system('beep -f %s -l %s' % (freq, duration))
+
+else:
+    def beep(freq, duration):
+        winsound.Beep(freq, duration)
+
+beep(WARNING_BEEP_DURATION[0], WARNING_BEEP_DURATION[1])
